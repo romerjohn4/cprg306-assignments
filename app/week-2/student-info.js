2f30fae2e1f15b7a6dc9e78da1fb44d14dd0e678
RomerJohn <romerjohn.delina@edu.sait.ca>
2024-09-11 13:45:11 -0600

    Added week 2 assignment
    I used Link to get to my GitHub and changed the root  for student info

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Romer De Lina</h1>
+        <p>
+        <Link href ="https://github.com/romerjohn4">My Gituhub Link</Link>
+        </p>
+      </main>
+     
+    );
+  }
